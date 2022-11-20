8a49acd8c8541f4d87e36ffabacec12eabe27ddd
BaraniukDmytro <79873346+BaraniukDmytro@users.noreply.github.com>
2022-11-20 23:46:15 +0200

    Update ProgramCodeModified.sql

--- 3-SoftwareConstruction/3.3-ContinuousIntegration/ProgramCodeModified.sql
+++ 3-SoftwareConstruction/3.3-ContinuousIntegration/ProgramCodeModified.sql
@@ -1,50 +1,13 @@
-/* Методи класу Post */
-CREATE OR REPLACE TYPE BODY Post AS 
+CREATE OR REPLACE TYPE Artist AS OBJECT( -- РєРѕСЂРёСЃС‚СѓРІР°С‡ С…СѓРґРѕР¶РЅРёРє
+    email VARCHAR(20), -- EMail
+    name_artist VARCHAR(100) -- С–Рј'СЏ РєРѕСЂРёСЃС‚СѓРІР°С‡Р°
+    telephone_number VARCHAR(20) -- С‚РµР»РµС„РѕРЅ
+    age_artist NUMBER(3), -- РІС–Рє
+    favorite_style VARCHAR(50) -- СѓР»СЋР±Р»РµРЅРёР№ СЃС‚РёР»СЊ 
+    /* РџСЂРѕС†РµРґСѓСЂР° РІРёРІРѕРґСѓ РЅР° РµРєСЂР°РЅ Р·РЅР°С‡РµРЅСЊ Р°С‚СЂРёР±СѓС‚С–РІ */
+    MEMBER PROCEDURE display
+);
 
-    /* конструктор екземплярів об'єктів класів.
-       	Вхідні параметри:
-	   1) p_post_mark - оцінка
-	   умова 1) якщо в таблиці Post вже існує вказана оцінка,
-	   значення збільшується на 1 
-	 Вихідний параметр - екземпляр обєкту класу 
-         */
-    CONSTRUCTOR FUNCTION Post(p_post_mark NUMBER)
-       RETURN SELF AS RESULT,
-    IS
-        v_mark Post.mark%TYPE;
-    BEGIN
-        SELECT mark INTO v_mark
-        FROM Post
-        WHERE 
-            mark = p_post_mark;
-        SELF.mark := v_mark+1;
-        RETURN;
-    END Location;
-
-
-	/* Функції отримання значень атрибутів */
-	/* get_post_owner */
-    MEMBER FUNCTION get_post_owner
-	    RETURN NUMBER 
-	IS
-    BEGIN
-       RETURN SELF.post_owner;
-    END get_post_owner;
-
-	/* get_content */
-    MEMBER FUNCTION get_content
-	    RETURN VARCHAR 
-	IS
-    BEGIN
-       RETURN SELF.content;
-    END get_content;	
- 
-    MEMBER PROCEDURE display IS 
-    BEGIN 
-        dbms_output.put_line('Owner: ' || post_owner); 
-        dbms_output.put_line('' || content); 
-        dbms_output.put_line('Mark: ' || mark); 
-        dbms_output.put_line('Date: ' || post_date); 
-    END display; 
-END; 
+/* РЎС‚РІРѕСЂРµРЅРЅСЏ С‚РёРїСѓ РґР»СЏ Р·Р±РµСЂС–РіР°РЅРЅСЏ СЃРїРёСЃРєСѓ РєРѕСЂРёСЃС‚СѓРІР°С‡С–РІ - "РљРѕР»РµРєС†С–СЏ РµРєР·РµРјРїР»СЏСЂС–РІ РѕР±`С”РєС‚С–РІ РєР»Р°СЃСѓ Artist */
+CREATE TYPE Artist_List IS TABLE OF Artist;
 /
